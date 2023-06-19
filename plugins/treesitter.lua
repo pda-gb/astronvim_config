3121662dea752291c4e028ed6bce06ae89c7e4b6
Denis P.A <52175155+pda-gb@users.noreply.github.com>
2023-06-19 03:41:02 +0300

    подсветка синтаксиса языков

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,23 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      -- выбор языков для подсветки (ensure_installed - принудительная установка
+      -- даже если отсутсвует при каждом запуске)
+      "lua",
+      "python",
+      "bash",
+      "html",
+      "css",
+      "dockerfile",
+      "gitignore",
+      "javascript",
+      "json",
+      "regex",
+      "sql",
+      "toml",
+      "yaml"
+
+
     })
   end,
 }
