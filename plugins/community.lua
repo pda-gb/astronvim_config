b527037a473351421e86bf07f245cbee40e87364
Denis P.A. <52175155+pda-gb@users.noreply.github.com>
2023-11-01 00:19:21 +0300

    Котлин пакет
    Установка котлин пакета из астрокомьюнити

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  
+  -- подключаем котлин пакет
+  { import = "astrocommunity.pack.kotlin" },
 }
